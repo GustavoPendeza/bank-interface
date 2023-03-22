3749aac23fd3b27c4956ce5345256ea8ae3e700e
Gustavo S. Pendeza <g.pendeza@hotmail.com>
2023-03-22 19:56:19 -0300

    Arrumando Nomes

--- src/screens/Deals.tsx
+++ src/screens/Deals.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export function Shop() {
+export function Deals() {
     return (
         <View className='flex-1 bg-background items-center justify-center'>
-            <Text className='text-white text-2xl font-semibold'>Shop screen</Text>
+            <Text className='text-white text-2xl font-semibold'>Deals screen</Text>
         </View>
     )
 }
--- src/screens/Wallet.tsx
+++ src/screens/Wallet.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export function Investments() {
+export function Wallet() {
     return (
         <View className='flex-1 bg-background items-center justify-center'>
-            <Text className='text-white text-2xl font-semibold'>Investments screen</Text>
+            <Text className='text-white text-2xl font-semibold'>Wallet screen</Text>
         </View>
     )
 }
